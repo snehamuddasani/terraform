13ad390c31a1292fdf5aa9ee441ca16474eafcc1
vishnuvardhan1231 <vissne123@gmail.com>
2024-07-27 15:41:02 +0530

    Update main.tf

--- day-2module/main.tf
+++ day-2module/main.tf
@@ -2,11 +2,10 @@ provider "aws"{
 region="us-east-1"
 }
 resource "aws_instance" "demo" {
-count = 1
 ami =var.ami_id
 instance_type = var.instancetype
 key_name = var.keyvalue
 tags={
-Name="xyz"
+Name="qwertyu"
 }
 }
